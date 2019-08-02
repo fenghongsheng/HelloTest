d568a7ed8847b4e7d942f84f5f6f9c20bc09436a
LaoFeng <576390965@qq.com>
2019-08-02 16:44:44 +0800

    添加生活建议和未来3天预报功能

--- app/src/main/java/com/helloweather/android/WeatherActivity.java
+++ app/src/main/java/com/helloweather/android/WeatherActivity.java
@@ -1,16 +1,20 @@
 package com.helloweather.android;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.helloweather.android.hello_gson.Forecast;
 import com.helloweather.android.hello_gson.Lifestyle;
 import com.helloweather.android.hello_gson.Weather;
@@ -24,6 +28,7 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
+    ImageView bingPicImg;
     private ScrollView weatherLayout;
     private TextView titleLocation;
     private TextView titleUpdatTime;
@@ -37,6 +42,11 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         weatherLayout=findViewById(R.id.weather_layout);
         titleLocation=findViewById(R.id.title_location);
         titleUpdatTime=findViewById(R.id.title_update_time);
@@ -46,8 +56,15 @@ public class WeatherActivity extends AppCompatActivity {
         typeText = findViewById(R.id.type_text);
         brfText = findViewById(R.id.brf_text);
         txtText = findViewById(R.id.txt_text);
+        bingPicImg = findViewById(R.id.bing_pic_img);
         SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString=prefs.getString("weather",null);
+        String bingPic = prefs.getString("bing_pic", null);
+        if (bingPic != null) {
+            Glide.with(this).load(bingPic).into(bingPicImg);
+        } else {
+            loadBingPic();
+        }
         if(weatherString!=null){
             Weather weather= Utility.handleWeatherResponse(weatherString);
             showWeatherInfo(weather);
@@ -88,6 +105,30 @@ public class WeatherActivity extends AppCompatActivity {
                             }
                         }
                     });
+            }
+        });
+    }
+
+    private void loadBingPic() {
+        final String requestBingpic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingpic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic", bingPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bingPicImg);
+                    }
+                });
             }
         });
     }
